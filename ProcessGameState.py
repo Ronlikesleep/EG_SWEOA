6710856abd6649f60d0f307ab2974dda17ab4fad
Ronlikesleep <zhangshanrong@outlook.com>
2023-05-25 17:30:40 -0700

    2th
    almost solve the p2

--- ProcessGameState.py
+++ ProcessGameState.py
@@ -1,19 +1,19 @@
 import json
 
 import pandas as pd
-
+import matplotlib.pyplot as plt
 
 Z_MIN = 285
 Z_MAX = 421
 
-
 class ProcessGameState:
 
     def __init__(self, filepath):
-        # Read the parquet file
+        # Read the parquet file with engine pyarrow and store the data in format of dataFrame
         self.data = pd.read_parquet("/Users/zhangshanrong/Desktop/game_state_frame_data.parquet", engine='pyarrow')
 
     def show_the_data(self, num_rows):
+        # show the first num of the data
         return self.data.head(num_rows)
 
     def is_point_in_triangle(pt, v1, v2, v3):
@@ -27,8 +27,8 @@ class ProcessGameState:
 
     def is_point_in_quadrilateral(pt, v1, v2, v3, v4):
         # For a point to be in a quadrilateral, it has to be in one of the two triangles that make up the quadrilateral
-        return ProcessGameState.is_point_in_triangle(pt, v1, v2, v3) or ProcessGameState.is_point_in_triangle(pt, v1,
-                                                                                                              v3, v4)
+        return ProcessGameState.is_point_in_triangle(pt, v1, v2, v3) \
+            or ProcessGameState.is_point_in_triangle(pt, v1, v3, v4)
 
     def is_point_on_line(pt, v1, v2):
         if pt == v1 or pt == v2:
@@ -55,16 +55,94 @@ class ProcessGameState:
                         ProcessGameState.is_point_on_line((row['x'], row['y']),*line_coords)), axis=1)
         return within_boundary
 
+
     def add_boundary_check_column(self, Z_MIN, Z_MAX):
         self.data['within_boundary'] = self.is_within_boundary(Z_MIN, Z_MAX)
 
+    def extract_weapon_classes(self):
+        def get_weapon_classes(inventory):
+            # If inventory is not None
+            if inventory is not None:
+                weapon_classes = []
+                # Iterate over each item in inventory
+                for item in inventory:
+                    # If 'weapon_class' is in the dictionary item, append it to weapon_classes
+                    if 'weapon_class' in item:
+                        weapon_classes.append(item['weapon_class'])
+                return weapon_classes
+            else:
+                return None
 
+        # Apply the function to each row in the 'inventory' column
+        self.data['weapon_classes'] = self.data['inventory'].apply(get_weapon_classes)
+
+    def plot_player_movements(self, round_num, team):
+        # Filter the data for the specific round and team
+        round_data = self.data[(self.data['round_num'] == round_num) & (self.data['team'] == team)]
+
+        # Get the unique players
+        players = round_data['player'].unique()
+
+        # Create a new figure
+        plt.figure(figsize=(10, 10))
+
+        # For each player
+        for player in players:
+            # Get the data for this player
+            player_data = round_data[round_data['player'] == player]
+
+            # Plot the player's movements
+            plt.scatter(player_data['x'], player_data['y'], label=player)
+
+        # Set the title, labels, and legend
+        plt.title(f'Player movements in round {round_num} for {team}')
+        plt.xlabel('x')
+        plt.ylabel('y')
+        plt.legend()
+        # Set x-axis range
+        plt.xlim(-4500, 0)
+        # Set y-axis range
+        plt.ylim(-4000, 1500)
+        # Show the plot
+        plt.savefig(f'/Users/zhangshanrong/Desktop/Team2/player_movements{round_num}.png')
+        plt.close()
+
+    def extract_specific_players(self, team, round_num, players):
+        # Filter the data
+        data_filtered = self.data[(self.data['team'] == team) & (self.data['round_num'] == round_num) & (self.data['player'].isin(players))]
+
+        # Select the columns x, y, z and within_boundary
+        data_selected = data_filtered[['x', 'y', 'z', 'within_boundary']]
+
+        return data_selected
+
+    def count_players_within_boundary(self, team, side):
+        # Filter the data
+        data_filtered = self.data[(self.data['team'] == team) & (self.data['side'] == side)]
+
+        data_grouped = data_filtered.groupby(['round_num', 'player'])
+        players_within_boundary = data_grouped['within_boundary'].any()
+
+        # For each round, count the number of players who ever entered the boundary
+        players_within_boundary_per_round = players_within_boundary.groupby(level=0).sum()
+
+        return players_within_boundary_per_round
 
 
 a = ProcessGameState("/Users/zhangshanrong/Desktop/game_state_frame_data.parquet")
+a.add_boundary_check_column(Z_MIN,Z_MAX)
+c = a.count_players_within_boundary("Team2", "T")
+print(c)
 
+
+#team2_players_within_boundary = count_players_within_boundary(a.data, 'Team2', 'T')
+#print(team2_players_within_boundary)
+"""for i in range(1,31):
+    a.plot_player_movements(i, 'Team2')"""
 # filtered_data = a.data[a.data['inventory'].notna()]
 # c = a.is_within_boundary(Z_MIN, Z_MAX)
+#a.extract_weapon_classes()
 
-a.data.to_excel("/Users/zhangshanrong/Desktop/1.xlsx", engine='openpyxl', index=False)
+
+#a.data.to_excel("/Users/zhangshanrong/Desktop/1.xlsx", engine='openpyxl', index=False)
 #print(a['weapon_classes'].head())
