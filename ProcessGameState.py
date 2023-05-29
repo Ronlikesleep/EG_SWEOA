28176a6e6075cc0cb4e12bac1f5ffffc297cc136
Ronlikesleep <zhangshanrong@outlook.com>
2023-05-28 22:33:36 -0700

    5/28 almost finished
    the code part is finished

--- ProcessGameState.py
+++ ProcessGameState.py
@@ -1,23 +1,28 @@
-import json
-
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
+from scipy.stats import gaussian_kde
+import numpy as np
 
+# Define z-axis boundaries of the chokepoint
 Z_MIN = 285
 Z_MAX = 421
 
 class ProcessGameState:
 
     def __init__(self, filepath):
+        #Q1 a
         # Read the parquet file with engine pyarrow and store the data in format of dataFrame
-        self.data = pd.read_parquet("/Users/zhangshanrong/Desktop/game_state_frame_data.parquet", engine='pyarrow')
+        self.data = pd.read_parquet(filepath, engine='pyarrow')
 
     def show_the_data(self, num_rows):
         # show the first num of the data
-        return self.data.head(num_rows)
+        return print(self.data.head(num_rows))
 
+    @staticmethod
     def is_point_in_triangle(pt, v1, v2, v3):
-        # Barycentric technique
+        # Check if point is inside triangle using barycentric technique
+        # For a point to be inside, all alpha, beta, gamma need to be positive
         alpha = ((v2[1] - v3[1]) * (pt[0] - v3[0]) + (v3[0] - v2[0]) * (pt[1] - v3[1])) / (
                     (v2[1] - v3[1]) * (v1[0] - v3[0]) + (v3[0] - v2[0]) * (v1[1] - v3[1]))
         beta = ((v3[1] - v1[1]) * (pt[0] - v3[0]) + (v1[0] - v3[0]) * (pt[1] - v3[1])) / (
@@ -25,12 +30,16 @@ class ProcessGameState:
         gamma = 1.0 - alpha - beta
         return alpha > 0 and beta > 0 and gamma > 0
 
+    @staticmethod
     def is_point_in_quadrilateral(pt, v1, v2, v3, v4):
         # For a point to be in a quadrilateral, it has to be in one of the two triangles that make up the quadrilateral
         return ProcessGameState.is_point_in_triangle(pt, v1, v2, v3) \
             or ProcessGameState.is_point_in_triangle(pt, v1, v3, v4)
 
+    @staticmethod
     def is_point_on_line(pt, v1, v2):
+        # A point is on a line if the line's y-coordinate at the point's x-coordinate is approximately equal to the point's y-coordinate
+        # this function is used to find the points between the diagonal of quadrilateral should be considered as within the boundary
         if pt == v1 or pt == v2:
             return False
 
@@ -44,7 +53,11 @@ class ProcessGameState:
         return abs(y - pt[1]) < 1e-6
 
     def is_within_boundary(self, z_min, z_max):
-        # Define triangle and quadrilateral coordinates
+        # Q1 b
+        # Define triangle, quadrilateral and the diagonal coordinates
+        # the overall time complexity of is_within_boundary is O(n) and only use the standard Python libraries
+        # returns a pandas.Series object, which contains Boolean values
+        # indicating whether each row of the dataframe is within the boundary.
         tri_coords = [(-1735, 250), (-2024, 398), (-1565, 580)]  # Points 13, 14, 17
         quad_coords = [(-2024, 398), (-2806, 742), (-2472, 1233), (-1565, 580)]  # Points 14, 15, 16, 17
         line_coords = [(-2024, 398), (-1565, 580)]  # Points 14, 17
@@ -57,9 +70,12 @@ class ProcessGameState:
 
 
     def add_boundary_check_column(self, Z_MIN, Z_MAX):
+        # Add a new boolean column to the dataframe that indicates whether each row is within boundary
         self.data['within_boundary'] = self.is_within_boundary(Z_MIN, Z_MAX)
 
     def extract_weapon_classes(self):
+        # Q1 c
+        # Extract weapon classes from inventory
         def get_weapon_classes(inventory):
             # If inventory is not None
             if inventory is not None:
@@ -74,9 +90,11 @@ class ProcessGameState:
                 return None
 
         # Apply the function to each row in the 'inventory' column
+        # Add a new column called 'weapon_classes', each column is a list
         self.data['weapon_classes'] = self.data['inventory'].apply(get_weapon_classes)
 
     def plot_player_movements(self, round_num, team):
+        # This is a helper function to track the movement of each team
         # Filter the data for the specific round and team
         round_data = self.data[(self.data['round_num'] == round_num) & (self.data['team'] == team)]
 
@@ -100,7 +118,7 @@ class ProcessGameState:
         plt.ylabel('y')
         plt.legend()
 
-        # Set x-axis y-axis range to have a brief overlook about the movement
+        # Set x-axis y-axis range and obtain a concise overview of the movement that aligns with the map
         plt.xlim(-4500, 0)
         plt.ylim(-4000, 1500)
 
@@ -108,16 +126,9 @@ class ProcessGameState:
         plt.savefig(f'/Users/zhangshanrong/Desktop/Team2/player_movements{round_num}.png')
         plt.close()
 
-    def extract_specific_players(self, team, round_num, players):
-        # Filter the data
-        data_filtered = self.data[(self.data['team'] == team) & (self.data['round_num'] == round_num) & (self.data['player'].isin(players))]
-
-        # Select the columns x, y, z and within_boundary
-        data_selected = data_filtered[['x', 'y', 'z', 'within_boundary']]
-
-        return data_selected
-
-    def count_players_within_boundary(self, team, side):
+    def count_players_within_boundary_each_round(self, team, side):
+        # Q2 a
+        # This function calculates the number of players of a specific team and side who enter the boundary in each round.
         # Filter the data
         data_filtered = self.data[(self.data['team'] == team) & (self.data['side'] == side)]
 
@@ -129,9 +140,10 @@ class ProcessGameState:
 
         return players_within_boundary_per_round
 
-    # retrieve the whole row for the first two entry per round of Team2 on the T side
-    # where the player enters BombsiteB with a rifle or an SMG
+
     def get_first_two_entry_rows(self):
+        # Retrieve the whole row for the first two entry per round of Team2 on the T side
+        # where the player enters BombsiteB with a rifle or an SMG(but SMG only for CT)
         # Filter the data for Team2 on the T side
         team2_data = self.data[(self.data['team'] == 'Team2') & (self.data['side'] == 'T') &
                                (self.data['weapon_classes'].notna())]
@@ -145,7 +157,6 @@ class ProcessGameState:
             round_data = team2_data[(team2_data['round_num'] == round_num) & (team2_data['area_name'] == 'BombsiteB')]
 
             # Filter the data for players who entered BombsiteB with at least one rifle or SMG
-            # but SMG is only for CT
             round_entry_data = round_data[
                 round_data['weapon_classes'].apply(lambda x: any(weapon in ['Rifle', 'SMG'] for weapon in x))
             ]
@@ -160,13 +171,14 @@ class ProcessGameState:
 
         return first_two_entry_rows
 
-    def get_average_timer(self, first_two_entry_rows):
-        # Since the 'seconds' and 'clock_time' can be set
-        # use tick to calculate the timer
+    def get_average_timer(self):
+        # Q2 b
+        # Since the 'seconds' and 'clock_time' can be set, I will use tick to calculate the timer
         # calculate each round's start ticks
         def calculate_round_start_ticks(df):
             return df.groupby('round_num')['tick'].min()
 
+        first_two_entry_rows = self.get_first_two_entry_rows()
         # Group by round_num and calculate the average tick for each round
         average_ticks = first_two_entry_rows.groupby('round_num')['tick'].mean()
         round_start_ticks = calculate_round_start_ticks(self.data)
@@ -182,19 +194,44 @@ class ProcessGameState:
 
         return average_timer
 
-a = ProcessGameState("/Users/zhangshanrong/Desktop/game_state_frame_data.parquet")
-a.extract_weapon_classes()
-c = a.get_first_two_entry_rows()
-print(a.get_average_timer(c))
-#c.to_excel("/Users/zhangshanrong/Desktop/3.xlsx", engine='openpyxl', index=False)
+    def draw_heatmap(self):
+        # Q2 c
+        # This function generates a heatmap showing the density of positions of 'Team2' on the 'CT' side in 'BombsiteB'.
+        # The function calculates densities and identifies the top five density points overall.
+        # The heatmap is sliced along the z-axis, and the top five density points within each slice are highlighted.
+        ct_data = self.data[
+            (self.data['team'] == 'Team2') & (self.data['side'] == 'CT') & (self.data['area_name'] == 'BombsiteB')]
+
+        # Calculate densities and get overall top five density points
+        x = ct_data['x']
+        y = ct_data['y']
+        z = ct_data['z']
+        xy = np.vstack([x, y])
+        density = gaussian_kde(xy)(xy)
+        idxs_top5_density = np.argsort(density)[-5:]
+        top5_density_points = list(zip(x.iloc[idxs_top5_density], y.iloc[idxs_top5_density], z.iloc[idxs_top5_density]))
+
+        # Define z slices
+        z_slices = range(int(ct_data['z'].min()), int(ct_data['z'].max()), 100)
+
+        for z in z_slices:
+            # Get data in the current slice
+            ct_data_slice = ct_data[(ct_data['z'] >= z) & (ct_data['z'] < z + 100)]
+
+            # KDE plot
+            plt.figure(figsize=(10, 10))
+
+            # Generate KDE plot
+            ax = sns.kdeplot(data=ct_data_slice, x='x', y='y', fill=True, cmap='Reds', levels=100)
+
+            # Mark top five points of maximum density if they fall in the current slice
+            for point in top5_density_points:
+                if z <= point[2] < z + 100:
+                    ax.plot(point[0], point[1], 'bo')
+
+            plt.title(f'Heatmap of Team2 CT Side Positions in BombsiteB for Z slice {z}-{z + 100}')
+            plt.show()
+        return top5_density_points
 
 
-
-#team2_players_within_boundary = count_players_within_boundary(a.data, 'Team2', 'T')
-#print(team2_players_within_boundary)
-"""for i in range(1,31):
-    a.plot_player_movements(i, 'Team2')"""
-# filtered_data = a.data[a.data['inventory'].notna()]
-# c = a.is_within_boundary(Z_MIN, Z_MAX)
-#a.extract_weapon_classes()
 
--- main.py
+++ main.py
@@ -1,16 +1,49 @@
-# This is a sample Python script.
+import ProcessGameState
+# This is my filepath, you can change to whatever you want :)
+MY_FILEPATH = "/Users/zhangshanrong/Desktop/game_state_frame_data.parquet"
+# Define z-axis boundaries of the chokepoint
+Z_MIN = 285
+Z_MAX = 421
+def main():
+    gameState = ProcessGameState.ProcessGameState(MY_FILEPATH)
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+    # Q1 a. read the file
+    print("Q1 a")
+    gameState.show_the_data(5)
+
+    # Q1 b. Return whether each row falls within a provided boundary
+    gameState.is_within_boundary(Z_MIN, Z_MAX)
+    gameState.add_boundary_check_column(Z_MIN, Z_MAX)
+    num_within_boundary = gameState.data['within_boundary'].sum()
+    print("Q1 b")
+    print(f"There are {num_within_boundary} rows within the boundary")
+
+    # Q1 c.Extract the weapon classes from the inventory json column
+    gameState.extract_weapon_classes()
+    print("Q1 c")
+    print(gameState.data['weapon_classes'].head())
+
+    # Q2 a Is entering via the light blue boundary a common strategy used by Team2 on T (terrorist) side?
+    print("Q2 a")
+    within_boundary_each_round = gameState.count_players_within_boundary_each_round('Team2', 'T')
+    print(within_boundary_each_round[within_boundary_each_round > 0])
+    """From the result, we can see that there is only one round for Team 2 with 2 players alive enter the chokepoint
+        So, we can say that it is not a common strategy"""
+
+    # Q2 b What is the average timer that Team2 on T(terrorist)side enter “BombsiteB” with least 2 rifles or SMGs?
+    print("Q2 b")
+    print(f"The average timer is {gameState.get_average_timer()}")
+    """The average timer is 28.5625"""
+
+    # Q2 c Using the same data set, tell our coaching staff where you suspect them to be waiting inside “BombsiteB”
+    print("Q2 c")
+    print(f"The top five points that are stayed by players are {gameState.draw_heatmap()}")
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    main()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
